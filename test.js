c5b83c970abcbd308be66d0567898e4345ad3da8
chance-up <ccs6473@naver.com>
2022-07-31 18:00:24 +0900

    test

--- test.js
+++ test.js
@@ -5,3 +5,9 @@ module.exports.someFunction = function () {
     fs.writeFileSync('test.txt', 'hi')
 };
 
+// todo 1. 글꼴 및 색상 적용
+// todo 2. 자동화 스크립트 작성
+// todo 3. 어떤 정보를 뿌려줄지?
+// todo 4. git action test
+
+
